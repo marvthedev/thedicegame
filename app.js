3871755d902c75f2003fcc25aaa957c3c7f04527
Marvin Marinas <mlmarinas@gmail.com>
2019-09-22 23:50:35 -0600

    Added second dice and player can now input the winning score to determine winner

--- app.js
+++ app.js
@@ -13,25 +13,37 @@ GAME RULES:
 let scores,
     roundScore,
     currentPlayer,
+    inputScore,
     isPlaying,
     currentScoreP1 = document.querySelector('#current-0'),
     currentScoreP2 = document.querySelector('#current-1'),
     diceDOM = document.querySelector('.dice');
+    diceDOM2 = document.querySelector('.dice2');
     
 reset();
-hideDice();
+// hideDice();
 
 //Start a New Game
 document.querySelector('.btn-new').addEventListener('click', reset);
+
+//Input round
+document.querySelector('.input-score').addEventListener('submit', function() {
+    inputScore = document.querySelector('.input-score').value;
+    console.log(inputScore);
+});
 
 //Roll Dice Button
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (isPlaying = true) {
         let dice = Math.floor(Math.random() * 6) + 1;
+        let dice2 = Math.floor(Math.random() * 6) + 1;
+        diceDOM.style.display = 'block';
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
-        if (dice !== 1) {
+        diceDOM2.src = 'dice-' + dice2 + '.png';
+        if (dice !== 1 && dice2 !==1) {
             roundScore += dice;
+            roundScore += dice2;
             document.querySelector('#current-' + currentPlayer).innerHTML = roundScore; 
         } else {
             nextPlayer();
@@ -42,12 +54,13 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 //Hold Button
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (isPlaying = true) {
+        inputScore = document.querySelector('.input-score').value;
         scores[currentPlayer] += roundScore;
         document.querySelector('#score-' + currentPlayer).innerHTML = scores[currentPlayer];
-        if (scores[currentPlayer] >= 100) {
-            document.querySelector('#name-' + player).textContent = 'Winner!';
-            document.querySelector('.player-' + player + '-panel').classList.add('winner');
-            document.querySelector('.player-' + player + '-panel').classList.remove('active');
+        if (scores[currentPlayer] >= inputScore) {
+            document.querySelector('#name-' + currentPlayer).textContent = 'Winner!';
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.remove('active');
             isPlaying = false;
         } else {
             nextPlayer();
